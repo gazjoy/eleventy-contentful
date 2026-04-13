f5f9d56221b373dad3c124e4410b11b72dc6133a
Martin Jackson <martin.jackson@valtech.com>
2026-04-13 11:09:34 +0100

    Paginating news posts, for when there are more than 1000

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,20 +1,40 @@
 const { deliveryApiClient } = require("../lib/contentful/client");
 
+const PAGE_SIZE = 1000; // max allowed by Contentful, we will paginate if we have more news posts than this
+
 module.exports = async function () {
   console.log("Fetching news posts from Contentful...");
 
-  const response = await deliveryApiClient.getEntries({ 
+  const allItems = [];
+  let query = {
     content_type: "newsPost",
-    limit: 1000
-  });
-  //console.log(`*** Response: ${JSON.stringify(response)}`);
+    limit: PAGE_SIZE,
+    cursor: true,
+  };
 
-  const newsPosts = response.items
+  while (query) {
+    const response = await deliveryApiClient.getEntries(query);
+    allItems.push(...response.items);
+
+    const nextUrl = response.pages?.next;
+    if (!nextUrl) break;
+
+    console.log(`... fetched ${allItems.length} news posts so far, need to fetch more ...`);
+
+    const pageNext = new URL(nextUrl, "https://cdn.contentful.com").searchParams.get("pageNext");
+    query = pageNext
+      ? {
+          pageNext,
+          limit: PAGE_SIZE,
+        }
+      : null;
+  }
+
+  const newsPosts = allItems
     .map(i => mapNewsPost(i))
     .sort((a, b) => b.date - a.date); // sort newest first
-  //console.log(`*** Mapped, sorted news posts: ${JSON.stringify(newsPosts)}`);
 
-  console.log(`... fetched ${newsPosts.length} news posts.`);
+  console.log(`... done. ${newsPosts.length} news posts fetched.`);
   return newsPosts;
 };
 
